01b669d518833876f5f7cd13f9b99d880536a9e2
Kayleigh Watts <kwatts949@gmail.com>
2023-01-18 15:50:32 +0000

    Build out sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,57 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import memoji from '../../assets/images/MeMoji.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar =() => (
-<div className='nav-bar'>
-<h1>Sidebar</h1>
-  <Link className='logo' to='/'><img src={memoji}/></Link>
-  <h1>Kay Watts</h1>
-  <nav></nav>
-</div>
+const Sidebar = () => (
+  <div className="nav-bar">
+    <h1>Kay Watts</h1>
+    <Link className="logo" to="/">
+      <img src={memoji} />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon classname="icon" icon={faHome} color="white" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="white" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="white" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/kay-watts/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="white" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/kwatts949/"
+        >
+          <FontAwesomeIcon icon={faGithub} color="white" />
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 export default Sidebar
