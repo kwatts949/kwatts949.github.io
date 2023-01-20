42e7f12da42587379b8990b2cc50326c18fc22fd
Kayleigh Watts <kwatts949@gmail.com>
2023-01-20 10:42:21 +0000

    update contact text

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -45,15 +45,7 @@ const Contact = () => {
           />
         </h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          I love hearing from people, whether its about coding, cats or anything else. If you have something to say, get in touch!
         </p>
         <div className="contact-form">
           <form ref={form} onSubmit={sendEmail}>
